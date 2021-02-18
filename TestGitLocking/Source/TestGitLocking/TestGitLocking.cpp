9614fc711c055d5a7ab1c67c9483cbc385bab8e5
Nick Arthur <nick@genpopinteractive.com>
2021-02-17 23:28:21 -0500

    Init content

---
+++ TestGitLocking/Source/TestGitLocking/TestGitLocking.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TestGitLocking.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TestGitLocking, "TestGitLocking" );
---
+++ TestGitLocking/Source/TestGitLocking/TestGitLockingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TestGitLockingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTGITLOCKING_API ATestGitLockingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
